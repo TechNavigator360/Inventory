28355b16f9c39cd0a2760c67f40cce6b5b50b3c5
TechNavigator360 <24015067@student.hhs.nl>
2025-10-30 17:14:56 +0100

    refactor(domain): optimize SerializedItem structure and UML v2.6 alignment.

--- src/Inventory.Domain/Entities/SerializedItem.cs
+++ src/Inventory.Domain/Entities/SerializedItem.cs
@@ -2,9 +2,9 @@ namespace Inventory.Domain.Entities;
 
 public class SerializedItem
 {
-    private Guid _id;
-    private string _serialNumber;
-    private bool _isAvailable;
+    private readonly Guid _id;
+    private readonly string _serialNumber;
+    private readonly bool _isAvailable;
 
     public Guid Id => _id;
     public string SerialNumber => _serialNumber;
@@ -12,6 +12,12 @@ public class SerializedItem
 
     protected internal SerializedItem(Guid id, string serialNumber, bool isAvailable)
     {
+        if (id == Guid.Empty)
+            throw new ArgumentException("Id mag niet leeg zijn.", nameof(id));
+
+        if (string.IsNullOrWhiteSpace(serialNumber))
+            throw new ArgumentNullException(nameof(serialNumber));
+            
         _id = id;
         _serialNumber = serialNumber;
         _isAvailable = isAvailable;
