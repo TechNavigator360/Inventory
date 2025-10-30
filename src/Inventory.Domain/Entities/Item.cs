bf17d4509fa5226a84b67c1a6ca3358440bc07b3
TechNavigator360 <24015067@student.hhs.nl>
2025-10-30 15:53:00 +0100

    refactor(domain): align Item with v2.6 (readonly fields + constructor guards)

--- src/Inventory.Domain/Entities/Item.cs
+++ src/Inventory.Domain/Entities/Item.cs
@@ -2,12 +2,12 @@ namespace Inventory.Domain.Entities;
 
 public abstract class Item
 {
-    private Guid _id;
-    private string _sku;
-    private string _name;
-    private string _description;
-    private int _minQuantity;
-    private Location _location;
+    private readonly Guid _id;
+    private readonly string _sku;
+    private readonly string _name;
+    private readonly string _description;
+    private readonly int _minQuantity;
+    private readonly Location _location;
 
     public Guid Id => _id;
     public string Sku => _sku;
@@ -18,11 +18,13 @@ public abstract class Item
 
     protected Item(Guid id, string sku, string name, string description, int minQty, Location location)
     {
-        _id = id;
+        _id = id != Guid.Empty ? id : throw new ArgumentException("Id mag niet leeg zijn.", nameof(id));
         _sku = sku ?? throw new ArgumentNullException(nameof(sku));
         _name = name ?? throw new ArgumentNullException(nameof(name));
         _description = description ?? string.Empty;
         _minQuantity = minQty;
+        if (minQty < 0)
+            throw new ArgumentOutOfRangeException(nameof(minQty), "MinQuantity mag niet negatief zijn.");
         _location = location ?? throw new ArgumentNullException(nameof(location));
     }
 
@@ -35,6 +37,7 @@ public abstract class Item
     {
         return amount > 0 && amount <= GetCurrentQuantity();
     }
+
     public abstract int GetCurrentQuantity();
     public abstract bool RequiresSerialNumber();
     protected internal abstract void AdjustQuantity(int delta);
