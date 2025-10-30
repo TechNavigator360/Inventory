88c08781542258270291901328f48b1d17e16a93
TechNavigator360 <24015067@student.hhs.nl>
2025-10-30 18:02:50 +0100

    refactor(domain): add validation and immutability to Location for UML v2.6 alignment

--- src/Inventory.Domain/Entities/Location.cs
+++ src/Inventory.Domain/Entities/Location.cs
@@ -1,22 +1,27 @@
-namespace Inventory.Domain.Entities;
-
-public class Location
+namespace Inventory.Domain.Entities
 {
-    private Guid _id;
-    private string _name;
-    private int _capacity;
-
-    public Guid Id => _id;
-    public string Name => _name;
-    public int Capacity => _capacity;
-
-    public Location(Guid id, string name, int capacity)
+    public class Location
     {
-        if (capacity <= 0)
-            throw new ArgumentOutOfRangeException(nameof(capacity), "Capaciteit moet groter zijn dan nul.");
+        private readonly Guid _id;
+        private readonly string _name;
+        private readonly int _capacity;
 
-        _id = id;
-        _name = name;
-        _capacity = capacity;
+        public Guid Id => _id;
+        public string Name => _name;
+        public int Capacity => _capacity;
+
+        public Location(Guid id, string name, int capacity)
+        {
+            if (id == Guid.Empty)
+                throw new ArgumentException("Id mag niet leeg zijn.", nameof(id));
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException(nameof(name));
+            if (capacity <= 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity), "Capaciteit moet groter zijn dan nul.");
+
+            _id = id;
+            _name = name;
+            _capacity = capacity;
+        }
     }
 }
